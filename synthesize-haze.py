7b45ad60e4a846b6758149bc71b8f4b195a2110b
hoangth9 <hoangth9@viettel.com.vn>
2023-08-18 17:16:07 +0700

    Add process all image in folder

--- synthesize-haze.py
+++ synthesize-haze.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 def get_file_name(path):
     basename = os.path.basename(path)
@@ -25,47 +26,58 @@ def gen_haze(img, depth_img):
     
     return hazy
 
-fname = "val01-25-20-1-FRONT"
-fname = "1"
-path = f"data/{fname}.jpg"
+directory = 'data/'
+for root, directories, files in os.walk(directory):
+    for filename in files:
+    # Join the two strings in order to form the full filepath.
+        filepath = os.path.join(root, filename)
+        prefixDepth = '_depth'
+        if (prefixDepth not in filepath): 
+            #fname = "val01-25-20-1-FRONT"
+            #New: Get only image
+            fname = Path(filepath).stem
+            path = f"data/{fname}.jpg"
 
-print(fname)
+            print(fname)
 
-img = plt.imread(path)
+            img = plt.imread(path)
 
-depth_path = f"data/{fname}_depth.jpg"
-haze_path = f"data/{fname}_haze.jpg"
-depth_img = plt.imread(depth_path)
+            depth_path = f"data/{fname}_depth.jpg"
+            haze_path = f"data/{fname}_haze.jpg"
+            depth_img = plt.imread(depth_path)
 
-depth_img_3c = np.zeros_like(img)
-depth_img_3c[:,:,0] = depth_img
-depth_img_3c[:,:,1] = depth_img
-depth_img_3c[:,:,2] = depth_img
+            depth_img_3c = np.zeros_like(img)
+            depth_img_3c[:,:,0] = depth_img
+            depth_img_3c[:,:,1] = depth_img
+            depth_img_3c[:,:,2] = depth_img
 
-beta=0.95
-norm_depth_img = depth_img_3c/255
-trans = np.exp(-norm_depth_img*beta)
+            beta=0.95
+            norm_depth_img = depth_img_3c/255
+            trans = np.exp(-norm_depth_img*beta)
 
-beta2=3.0
-trans2 = np.exp(-norm_depth_img*beta2)
+            beta2=3.0
+            trans2 = np.exp(-norm_depth_img*beta2)
 
-A = 255
-hazy = img*trans + A*(1-trans)
-hazy = np.array(hazy, dtype=np.uint8)
+            A = 255
+            hazy = img*trans + A*(1-trans)
+            hazy = np.array(hazy, dtype=np.uint8)
 
-hazy2 = img*trans2 + A*(1-trans2)
-hazy2 = np.array(hazy2, dtype=np.uint8)
+            hazy2 = img*trans2 + A*(1-trans2)
+            hazy2 = np.array(hazy2, dtype=np.uint8)
 
-plt.figure(figsize=(16,18))
-plt.subplot(321), plt.imshow(img)
-plt.subplot(322), plt.imshow(depth_img, cmap="gray")
-plt.subplot(323), plt.imshow(trans, cmap="gray")
-plt.subplot(324), plt.imshow(hazy)
-plt.subplot(325), plt.imshow(trans2, cmap="gray")
-plt.subplot(326), plt.imshow(hazy2)
+            plt.figure(figsize=(16,18))
+            plt.subplot(321), plt.imshow(img)
+            plt.subplot(322), plt.imshow(depth_img, cmap="gray")
+            plt.subplot(323), plt.imshow(trans, cmap="gray")
+            plt.subplot(324), plt.imshow(hazy)
+            plt.subplot(325), plt.imshow(trans2, cmap="gray")
+            plt.subplot(326), plt.imshow(hazy2)
 
-#New: Save haze image
-plt.imsave(haze_path, hazy)
+            #New: Save haze image
+            plt.imsave(haze_path, hazy)
 
-plt.show()
+            #plt.show()
+
+
+
 
